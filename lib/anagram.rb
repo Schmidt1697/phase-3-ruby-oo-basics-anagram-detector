c7884280907dc4d476fdf594e4a6946c349fb269
Anna Schmidt <marshall1697@gmail.com>
2022-10-11 18:32:20 -0600

    commit tree da42b5928cb82d5b8f5fbd83a5602cd7ed5e3b01

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
 # Your code goes here!
+require "pry"
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(comparison)
+        
+        comp_chars_arr = comparison.map do |word|
+           word.chars
+        end
+
+        matched_words = comp_chars_arr.filter do |comp|
+            comp.sort == @word.chars.sort
+        end
+
+        matched_arr = matched_words.map do |word|
+            word.join('')
+        end
+    
+    end
+
+end
+
+care = Anagram.new('gallery')
+# care.match('race')
+care.match(["gallery", "regally", "largely"])
+
+# binding.pry
